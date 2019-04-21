626364f12cf5ed2fec3942748e2ffd1ac693ab1d
changpei <changpei@meicai.cn>
2019-04-21 10:47:38 +0800

    build

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: {
     autoprefixer: {}
-  }
+  },
+  outputDir: path.resolve(__dirname, './docs'),
+  // indexPath: path.resolve(__dirname, '../views/site/index.php')
 }
